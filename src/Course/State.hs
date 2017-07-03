5734fc7c055d85194a37f1db58ea9da376f0ca9e
Andrew McCluskey <andrew.mccluskey@csiro.au>
2017-07-03 16:06:57 +1000

    Hide Course.Monad.(<*>)

--- src/Course/State.hs
+++ src/Course/State.hs
@@ -12,7 +12,7 @@ import Course.Optional
 import Course.List
 import Course.Functor
 import Course.Applicative hiding ((<$>))
-import Course.Monad
+import Course.Monad hiding ((<*>))
 import qualified Data.Set as S
 
 -- $setup
