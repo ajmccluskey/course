0871d893103461556bc50ff550311a0177e6da6b
Andrew McCluskey <andrew@ajmccluskey.com>
2018-02-05 16:08:55 +1000

    Add .dir-locals.el

---
+++ src/Course/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (dante-repl-command-line . ("nix-shell" "--run" "ghci -fno-code"))))
