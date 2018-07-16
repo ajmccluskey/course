f9338a96e48240319bffaa1201b9b5b2faa23519
Andrew McCluskey <andrew@ajmccluskey.com>
2018-07-16 16:34:37 +1000

    WIP on Tasty instance for Mini
    probs not working after changes to class

--- test/Test/Tasty/Mini.hs
+++ test/Test/Tasty/Mini.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE ImplicitPrelude       #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE RankNTypes            #-}
+{-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
 
 module Test.Tasty.Mini where
 
-import qualified Test.Tasty as T
-import           Test.Tasty.HUnit as T
+import qualified Test.Tasty            as T
+import qualified Test.Tasty.HUnit      as T
+import qualified Test.Tasty.QuickCheck as T
 
-import           Test.Mini        (Tester (..), UnitTester (..))
+import           Test.Mini             (PropertyTester (..), Tester (..),
+                                        UnitTester (..))
 
 
 newtype TastyAssertion =
@@ -18,17 +21,21 @@ newtype TastyAssertion =
 newtype TastyTree =
   TT {getTT :: T.TestTree}
 
-instance Tester TastyTree T.TestName TastyAssertion where
+instance Tester TastyTree T.TestName where
   testGroup n =
     TT . T.testGroup n . fmap getTT
 
+  test = T.defaultMain . getTT
+
+instance UnitTester TastyTree T.TestName TastyAssertion where
   testCase n =
     TT . T.testCase n . getTA
 
-  test = T.defaultMain . getTT
-
-instance UnitTester TastyAssertion where
   (@?=) = (TA .) . (T.@?=)
+
+instance T.Testable a => PropertyTester TastyTree T.TestName a where
+  testProperty n =
+    TT . T.testProperty n
 
 
 tastyTest ::
